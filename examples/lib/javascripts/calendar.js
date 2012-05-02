78bcbe5b55b1e15a326070167e78880667765ee4
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2012-05-02 15:22:58 +0200

    Added ajaxLink for number of patients booked per day on calendar control

--- examples/lib/javascripts/calendar.js
+++ examples/lib/javascripts/calendar.js
@@ -255,7 +255,10 @@ function createMonth(date, selected, initialdate, startweekdate, endweekdate){
                     dayCell.setAttribute("value", current_day.getDate());
                             
                     dayCell.setAttribute("daytype", "weekday");
-                                        
+                              
+                    dayCell.setAttribute("date", current_day.getFullYear() + "-" + padZeros((current_day.getMonth()+1),2) + 
+                        "-" + padZeros(current_day.getDate(), 2));          
+                              
                     dayCell.className = "cCell day cellActive" + 
                     ((current_day.getFullYear() == today.getFullYear() && 
                         current_day.getMonth() == today.getMonth() && 
@@ -263,7 +266,7 @@ function createMonth(date, selected, initialdate, startweekdate, endweekdate){
                     
                     dayCell.onclick = function(){
                         var celldate = current_day.getFullYear() + "-" + padZeros((current_day.getMonth()),2) + 
-                        "-" + padZeros(__$(active_cell).getAttribute("value"));
+                        "-" + padZeros(__$(active_cell).getAttribute("value"), 2);
                         
                         if(celldate == date){
                             __$(active_cell).style.backgroundColor = initial_selected_color;
@@ -287,13 +290,19 @@ function createMonth(date, selected, initialdate, startweekdate, endweekdate){
                         padZeros(this.getAttribute("value"), 2);
                                 
                         selected_date = new Date(current_year, months[current_month][0], this.getAttribute("value"));
+                        
+                        if(targetControl.getAttribute("ajaxCalendarUrl")){
+                            ajaxCalendarRequest(__$(this.id), targetControl.getAttribute("ajaxCalendarUrl"), 
+                                __$(this.id).getAttribute("date"));
+                        }
                     }
                 } else {                                        
                     dayCell.className = "cCell day cellInactive";
                     dayCell.style.color = "#999";
                 }
                 
-                current_day.setDate(current_day.getDate() + 1);
+                current_day.setDate(current_day.getDate() + 1); 
+                
             }
             if(found){
                 __$(active_cell).click();
@@ -302,7 +311,7 @@ function createMonth(date, selected, initialdate, startweekdate, endweekdate){
         
         carry_over_day = new Date(current_day.getFullYear(), current_day.getMonth(), current_day.getDate());
     }
-      
+     
 }
                         
 function createCalendar(control, target, date, selected, startweekdate, endweekdate){
@@ -360,5 +369,41 @@ function createCalendar(control, target, date, selected, startweekdate, endweekd
         mainrow2.appendChild(mainrow2cell);
         
         createMonth(date, selected, date, startweekdate, endweekdate);
+    }
+}
+
+function ajaxCalendarRequest(aElement, aUrl, date) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        handleCalendarResult(aElement, httpRequest, date);
+    };
+    try {
+        httpRequest.open('GET', aUrl, true);
+        httpRequest.send(null);
+    } catch(e){
+    }
+}
+
+function handleCalendarResult(element, aXMLHttpRequest, date) {
+    if (!aXMLHttpRequest) return;
+
+    if (!element) return;
+
+    if (aXMLHttpRequest.readyState == 4 && aXMLHttpRequest.status == 200) {
+        var result = JSON.parse(aXMLHttpRequest.responseText);                
+        
+        console.log(date);
+        
+        if(result[date]){
+        
+            if(element.getElementsByTagName("div").length <= 0){                    
+                var count = document.createElement("div");
+                count.className = "dayCount";
+                        
+                element.appendChild(count);
+            }
+        
+            element.getElementsByTagName("div")[0].innerHTML = result[date];
+        }
     }
 }
