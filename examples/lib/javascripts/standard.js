05c107e77af6c496323ec49db9e4fee28f581f2e
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2012-07-24 10:55:27 +0200

    Adopted changes from machine change and fixed unknown button bug

--- examples/lib/javascripts/standard.js
+++ examples/lib/javascripts/standard.js
@@ -2153,6 +2153,10 @@ function press(pressedChar){
     if (singleButtonMode)
         inputTarget.value = "";
 
+    var unknownClickedEarlier = inputTarget.value.toLowerCase();
+    if (unknownClickedEarlier == "unknown")
+        inputTarget.value = "";
+
     if (pressedChar.length == 1) {
         inputTarget.value += getRightCaseValue(pressedChar);
 
