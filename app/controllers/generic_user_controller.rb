b55694e53902898c8d28f99ecc2c545cbdfcec47
Deliwe <dbnkhoma@gmail.com>
2012-08-07 11:56:56 +0200

    updated user controller and it's views

--- app/controllers/generic_user_controller.rb
+++ app/controllers/generic_user_controller.rb
@@ -234,17 +234,17 @@ class GenericUserController < ApplicationController
         flash[:notice] = "You have successfuly added the role of #{params[:user_role][:role_id]}"
         redirect_to :action => "show"
       else
-      user_roles = UserRole.find_all_by_user_id(@user.user_id).collect{|ur|ur.role.role}
+      user_roles = UserRole.find_all_by_user_id(@user.user_id).collect{|ur|ur.role}
       all_roles = Role.find(:all).collect{|r|r.role}
       @roles = (all_roles - user_roles)
-      @show_super_user = true if UserRole.find_all_by_user_id(@user.user_id).collect{|ur|ur.role.role != "superuser" }
+      @show_super_user = true if UserRole.find_all_by_user_id(@user.user_id).collect{|ur|ur.role != "superuser" }
    end
   end
 
   def delete_role
     @user = User.find(params[:id])
     unless request.post?
-      @roles = UserRole.find_all_by_user_id(@user.user_id).collect{|ur|ur.role.role}
+      @roles = UserRole.find_all_by_user_id(@user.user_id).collect{|ur|ur.role}
     else
       role = Role.find_by_role(params[:user_role][:role_id]).role
       user_role =  UserRole.find_by_role_and_user_id(role,@user.user_id)  
