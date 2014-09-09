924faa0dc9d6f98fbe04501e70d0c5f1c1ac5083
Blessings <blessings.mhango@gmail.com>
2014-09-09 08:38:15 +0200

    added conditions

--- script/install.sh
+++ script/install.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
-
-IFS=$'\n' read -d '' -r -a requirements < /var/www/Registration/config/application_requirements.yml
-
+   # reading application configuration file
+   IFS=$'\n' read -d '' -r -a requirements < /var/www/Registration/config/application_requirements.yml
+   
+   # initialize variables
    RB_VERSION=`echo "${requirements[0]}" | cut -d':' -f2`
    RG_VERSION=`echo "${requirements[1]}" | cut -d':' -f2`
    DB_NAME=`echo "${requirements[2]}" | cut -d':' -f2`
    BD_VERSION=`echo "${requirements[3]}" | cut -d':' -f2`
+   
+   # check already installed requirements
+   RB_INSTALLED_VERSION=`echo "$(ruby -e 'print RUBY_VERSION')"` 
+   RG_INSTALLED_VERSION=`echo "$(gem -v)"`
 
-   RB_INSTALLED_VERSION=`echo "$(ruby -e 'print RUBY_VERSION')"`
-   RG_INSTALLED_VERSION=`echo "$(gem -v)"` 
+    
+   if [ -f /etc/init.d/mysql* ]; then
+     FIRST_DB_INSTALLED_NAME='mysql'
+   else 
+     FIRST_DB_INSTALLED_NAME='mysql not installed'
+   fi
+
+   if [ -f /usr/bin/couchdb ]; then
+     SECOND_DB_INSTALLED_NAME='couchdb'
+   else 
+     SECOND_DB_INSTALLED_NAME='couchdb not installed'
+   fi
+
+   BUNDLER_INSTALLED_VERSION=`echo "$(gem list -i bundler --version 1.6.7)"`
+
+   if $BUNDLER_INSTALLED_VERSION; then
+     BD_INSTALLED_VERSION='1.6.2'
+   else 
+     BD_INSTALLED_VERSION='not correct version'
+   fi
+    
 
 echo  $RB_VERSION
 echo  $RG_VERSION
@@ -17,5 +41,8 @@ echo  $BD_VERSION
 
 echo $RB_INSTALLED_VERSION
 echo $RG_INSTALLED_VERSION
+echo $FIRST_DB_INSTALLED_NAME
+echo $SECOND_DB_INSTALLED_NAME
+echo $BD_INSTALLED_VERSION
 
 
