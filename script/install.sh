b57c546675c578a4d96b31f114717a2769ac82c8
Blessings <blessings.mhango@gmail.com>
2014-09-09 06:32:45 +0200

    reading ruby environment

--- script/install.sh
+++ script/install.sh
@@ -2,7 +2,20 @@
 
 IFS=$'\n' read -d '' -r -a requirements < /var/www/Registration/config/application_requirements.yml
 
-for r in "${requirements[@]}"
-do
-   echo "$r"
-done
+   RB_VERSION=`echo "${requirements[0]}" | cut -d':' -f2`
+   RG_VERSION=`echo "${requirements[1]}" | cut -d':' -f2`
+   DB_NAME=`echo "${requirements[2]}" | cut -d':' -f2`
+   BD_VERSION=`echo "${requirements[3]}" | cut -d':' -f2`
+
+   RB_INSTALLED_VERSION=`echo "$(ruby -e 'print RUBY_VERSION')"`
+   RG_INSTALLED_VERSION=`echo "$(gem -v)"` 
+
+echo  $RB_VERSION
+echo  $RG_VERSION
+echo  $DB_NAME
+echo  $BD_VERSION
+
+echo $RB_INSTALLED_VERSION
+echo $RG_INSTALLED_VERSION
+
+
