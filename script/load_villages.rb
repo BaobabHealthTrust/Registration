e84fbd2cfa21d549d5e4d0a3893577ba0eb0b24d
Blessings <blessings.mhango@gmail.com>
2016-01-16 15:08:57 +0200

    script for loading villages

---
+++ script/load_villages.rb
@@ -0,0 +1,22 @@
+require 'fastercsv'
+
+FasterCSV.foreach(Rails.root.join('db','missing_villages.csv')) do |row|
+ district = DDEDistrict.find_by_name(row[0]) 
+ if district.blank? 
+  next
+ end 
+ ta = DDETraditionalAuthority.find_by_name_and_district_id(row[1],district.district_id)
+ 
+ if ta.blank? 
+  next
+ end 
+ 
+ village = DDEVillage.find_by_name_and_traditional_authority_id(row[2], ta.traditional_authority_id)
+  
+ if village.blank?
+   DDEVillage.create(:name => row[2], :traditional_authority_id => ta.traditional_authority_id, :creator => 1, :date_created => Date.today)
+   puts "#{row[2]} created." 
+ else
+   puts "#{row[2]} already exists." 
+ end
+end
