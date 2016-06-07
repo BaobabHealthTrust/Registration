cbcd7f928330e75d38acc5c71efb3d86a24fa5bd
Blessings <blessings.mhango@gmail.com>
2016-06-07 15:33:55 +0200

    working on cohort

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -86,54 +86,19 @@ class Cohort
                                 end
 	end
 	
-	
-	def referrals(start_date = @start_date, end_date = @end_date)
-
-    services_concept_id = ConceptName.find_by_name('SERVICES').concept_id
+  def referrals(start_date = @start_date, end_date = @end_date)
+    outpatient_reception_encounter = EncounterType.find_by_name("OUTPATIENT RECEPTION").id
+    referred_from_concept = ConceptName.find_by_name("REFERRED FROM").concept_id
     
-    registration_services_hash = {} ; services = []
-    registration_services_hash['SERVICES'] = {'Casualty' => 0,'Dental' => 0,'Eye' => 0,'Family Planing' => 0,'Medical' => 0,'OB/Gyn' => 0,'Orthopedics' => 0,'Other' => 0,'Pediatrics' => 0,'Skin' => 0,'STI Clinic' => 0,'Surgical' => 0} 
-
-    services = PatientProgram.find_by_sql("SELECT patient_id,value_text services,obs_datetime FROM obs
-                                INNER JOIN patient p ON p.patient_id = obs.person_id
-                                INNER JOIN concept_name n ON n.concept_id = obs.concept_id
-                                WHERE obs.obs_datetime >='#{start_date}'
-                                AND obs.obs_datetime <= '#{end_date}' 
-                                AND obs.concept_id = #{services_concept_id}
-                                AND n.name != ''
-                                GROUP BY obs.obs_id").map{ | value | value.services }
-                                
-		( services || [] ).each do | service |
-				  if service == 'Casualty'
-				    registration_services_hash['SERVICES']['Casualty'] += 1
-				  elsif service == 'Eye'
-				    registration_services_hash['SERVICES']['Eye'] += 1
-				  elsif service == 'Family Planing'
-				    registration_services_hash['SERVICES']['Family Planing'] += 1
-				  elsif service == 'Dental'
-				    registration_services_hash['SERVICES']['Dental'] += 1
-				  elsif service == 'Medical'
-				    registration_services_hash['SERVICES']['Medical'] += 1
-				  elsif service == 'OB/Gyn'
-				    registration_services_hash['SERVICES']['OB/Gyn'] += 1
-				  elsif service == 'Orthopedics'
-				    registration_services_hash['SERVICES']['Orthopedics'] += 1
-				  elsif service == 'Pediatrics'
-				    registration_services_hash['SERVICES']['Pediatrics'] += 1
-				  elsif service == ' Skin '
-				    registration_services_hash['SERVICES']['Skin'] += 1
-				  elsif service == 'STI Clinic'
-				    registration_services_hash['SERVICES']['STI Clinic'] += 1
-				  elsif service == 'Surgical'
-				    registration_services_hash['SERVICES']['Surgical'] += 1
-				  else
-						registration_services_hash['SERVICES']['Other'] += 1
-				  end
-				end
-				registration_services_hash
+    Observation.find_by_sql("SELECT l.name lname, COUNT(l.name) lcount FROM obs o INNER JOIN encounter e 
+														ON o.encounter_id = e.encounter_id
+														INNER JOIN location l
+														ON l.location_id = o.value_text
+														WHERE e.encounter_type = #{outpatient_reception_encounter} 
+														AND o.concept_id = #{referred_from_concept} 
+														AND e.date_created BETWEEN '#{start_date}' AND '#{end_date}' 
+														GROUP BY l.name;").map{ | value | {value.lname => value.lcount} }
+		
   end
-
-	
-	
 	
 end
